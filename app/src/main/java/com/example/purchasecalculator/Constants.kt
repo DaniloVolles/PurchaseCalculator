607f35f8be32720437af4cdeaa7685543896c6b1
Danilo Volles <danilo.volles@outlook.com>
2023-11-27 20:37:00 -0300

    Type constants implementation.

---
+++ app/src/main/java/com/example/purchasecalculator/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.purchasecalculator
+
+class Constants private constructor(){
+
+    object TYPE {
+        const val LITERS    = 1
+        const val METERS    = 2
+        const val UNITIES   = 3
+        const val GRAMS     = 4
+    }
+}
