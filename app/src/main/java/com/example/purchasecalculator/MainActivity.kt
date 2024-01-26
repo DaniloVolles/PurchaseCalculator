038957bf7f8c6906cd45b3d411b5da01ac3a43bc
Danilo Volles <danilo.volles@outlook.com>
2024-01-26 16:56:56 -0300

    Interface updates.

--- app/src/main/java/com/example/purchasecalculator/MainActivity.kt
+++ app/src/main/java/com/example/purchasecalculator/MainActivity.kt
@@ -2,6 +2,8 @@
 
 package com.example.purchasecalculator
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,61 +11,41 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.purchasecalculator.constants.Constants
 import com.example.purchasecalculator.model.Product
 import com.example.purchasecalculator.model.ProductMock
-import com.example.purchasecalculator.ui.theme.PurchaseCalculatorTheme
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
 
 
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                Modifier
-                    .fillMaxSize()
-                    .padding(20.dp),
-                verticalArrangement = Arrangement.Bottom,
-                horizontalAlignment = Alignment.End
-            ) {
-                ProductList(ProductMock.productList)
-                FloatingActionButton()
-            }
-
-//            PurchaseCalculatorTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//
-////                    ProductList()
-//                    FloatingActionButton()
-//
-//                }
-//            }
+            MyApp(this)
         }
     }
 
@@ -71,14 +53,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ProductList(products: List<Product>) {
-    Surface (Modifier.fillMaxSize()){
-        LazyColumn {
-            items(products){
-                ProductRow(product = it)
-            }
-        }
-        FloatingActionButton()
-    }
+
 }
 
 @Composable
@@ -150,26 +125,50 @@ fun PreviewProductRow() {
 }
 
 @Composable
-fun FloatingActionButton() {
+fun MyApp(context: Context) {
 
-    val activity = LocalContext.current
+    val scope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    FloatingActionButton(
-        onClick = { activity.startActivity(Intent(activity, FormActivity::class.java)) },
-        modifier = Modifier
-            .padding(32.dp)
-            .wrapContentSize()
-    ) {
-        Icon(
-            painter = painterResource(id = R.drawable.ic_add),
-            contentDescription = null,
-            modifier = Modifier.size(24.dp)
-        )
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        bottomBar = {
+            BottomAppBar(
+                actions = {
+                    IconButton(onClick = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar("Aqui será implementada a limpeza do BD.") }
+                    }) {
+                        Icon(
+                            painterResource(id = R.drawable.ic_delete),
+                            contentDescription = null
+                        )
+                    }
+                },
+                floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {
+                            val intent = Intent(context, FormActivity::class.java)
+                            context.startActivity(intent)
+                        },
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_add),
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(contentPadding = innerPadding) {
+            items(ProductMock.productList) {
+                ProductRow(product = it)
+            }
+        }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewFloatingActionButton() {
-    FloatingActionButton()
-}
